bb621dc9fe7ba37ba00359ecfc2111a5877192e8
Nikki <piknik@me.com>
2020-12-28 14:24:48 -0700

    works better now and is spaced nicely

--- main.js
+++ main.js
@@ -1,21 +1,25 @@
-var storySelector = document.querySelector('section.story');
-var formSelector = document.querySelector('section.form');
+var storySelector = document.querySelector('div.story');
+var formSelector = document.querySelector('div.form');
 var refreshSelector = document.getElementById('start-over');
 var submitButton = document.getElementById('write');
-var noun1 = document.getElementById('noun1');
-var noun2 = document.getElementById('noun2');
-var place1 = document.getElementById('place1');
-var num1 = document.getElementById('num1');
-var verb1 = document.getElementById('verb1');
-var clothing1 = document.getElementById('clothing1');
-var clothing2 = document.getElementById('clothing2');
-var beverage1 = document.getElementById('beverage1');
-var food1 = document.getElementById('food1');
 
 submitButton.addEventListener('click', function(e) {
   e.preventDefault();
+  var userInputs = document.getElementsByClassName('inputs');
 
   toggle();
+
+  document.getElementById('noun1Text').innerText = userInputs[0].value;
+  document.getElementById('place1Text').innerText = userInputs[1].value;
+  document.getElementById('num1Text').innerText = userInputs[2].value;
+  document.getElementById('noun2Text').innerText = userInputs[3].value;
+  document.getElementById('verb1Text').innerText = userInputs[4].value;
+  document.getElementById('clothing1Text').innerText = userInputs[5].value;
+  document.getElementById('clothing2Text').innerText = userInputs[6].value;
+  document.getElementById('beverage1Text').innerText = userInputs[7].value;
+  document.getElementById('food1Text').innerText = userInputs[8].value;
+
+  document.getElementById('form-reset').reset();
 });
 
 refreshSelector.addEventListener('click', function(e) {
